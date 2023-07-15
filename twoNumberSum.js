f5c0082c0a5ee758129fa897b8da103f417edf12
umraooo7 <aku.kiet@gmail.com>
2023-07-15 15:23:51 +0530

    refining solution

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -1,5 +1,9 @@
+// solution 1
+
 function twoNumberSum(array, targetSum) {
-  // Write your code here.
+  if (array.length <= 0) {
+    return [];
+  }
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.legth; j++) {
       if (array[i] + array[j] === targetSum) {
@@ -7,6 +11,7 @@ function twoNumberSum(array, targetSum) {
       }
     }
   }
+  return [];
 }
 
 console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
