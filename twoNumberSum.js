c3ef54194698ab42e22974c7a0fd90a7e8ad7b57
deepakumrao <deepakumrao45@gmail.com>
2023-07-15 21:21:16 +0530

    twoNumberSum problem with hashMap

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -7,7 +7,7 @@ function twoNumberSum(array, targetSum) {
     return [];
   }
   for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.legth; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       if (array[i] + array[j] === targetSum) {
         return [array[i], array[j]];
       }
@@ -15,5 +15,26 @@ function twoNumberSum(array, targetSum) {
   }
   return [];
 }
-
 console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
+
+// solution 2
+// time and space complexcity O(N)
+
+function twoNumberSumWithHashMap(array, targetSum) {
+  if (array.length <= 0) {
+    return [];
+  }
+  let uniqueNumberSet = new Set();
+  let = i = 0;
+  while (i < array.length) {
+    let numberToCompare = targetSum - array[i];
+    if (uniqueNumberSet.has(numberToCompare)) {
+      return [numberToCompare, array[i]];
+    } else {
+      uniqueNumberSet.add(array[i]);
+      i++;
+    }
+  }
+  return [];
+}
+console.log(twoNumberSumWithHashMap([3, 5, -4, 8, 11, 1, -1, 6], 10));
