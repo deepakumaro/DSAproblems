c67c1ed1061c02430694d21175b20e576330a512
deepakumrao <deepakumrao45@gmail.com>
2023-07-22 12:16:18 +0530

    problem explained

--- string/easy/semordnilap.js
+++ string/easy/semordnilap.js
@@ -1,3 +1,12 @@
+// word in words should have a pair in the array. you can not just reverse the word and check in the same array
+/* 
+check this with this output 
+{
+  "words": ["aaa", "bbbb"]
+}
+output - [];
+ */
+
 function semordnilap(words) {
   // Write your code here.
   const hashMap = new Set();
@@ -8,6 +17,25 @@ function semordnilap(words) {
       semordnilapArray.push([word, reversed]);
     } else {
       hashMap.add(word);
+    }
+  }
+
+  return semordnilapArray;
+}
+
+console.log(semordnilap(["dog", "desserts", "god", "stressed"]));
+
+// sol 2
+function semordnilap(words) {
+  // Write your code here.
+  const hashMap = new Set(words);
+  const semordnilapArray = [];
+  for (let word of words) {
+    let reversed = word.split("").reverse().join("");
+    if (hashMap.has(reversed) && reversed !== word) {
+      semordnilapArray.push([word, reversed]);
+      hashMap.delete(word);
+      hashMap.delete(reversed);
     }
   }
 
